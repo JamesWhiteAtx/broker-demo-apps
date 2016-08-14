63dac7be3728e46bc88b4c5b4220b01dab5ed385
jameswhite <jameswhiteatx@gmail.com>
2016-08-14 13:10:43 -0500

    Fixed storage serveice remove key.

--- src/demos/shared/storage.service.ts
+++ src/demos/shared/storage.service.ts
@@ -11,23 +11,29 @@ export class StorageService {
   constructor(@Inject('Window') private window: Window) { }
 
   set(key: string, obj: Object) {
-    this.setMulti(key, obj);
+    this.setMultiVal(key, obj);
   }
   get<T extends Object>(key: string): T {
     let multi = this.getMulti();
     return <T>multi[key];
   }
   remove(key: string) {
-    return this.window.sessionStorage.removeItem(key);
+    this.clearMultiVal(key);
   }
 
   private getMulti(): KeyHash {
     return this.getObject<KeyHash>(MULTI_STORE_KEY) || new KeyHash();
   }
 
-  private setMulti(key: string, obj: Object): void {
+  private setMultiVal(key: string, obj: Object): void {
     let multi = this.getMulti();
     multi[key] = obj;
+    this.setObject(MULTI_STORE_KEY, multi);
+  }
+
+  private clearMultiVal(key: string): void {
+    let multi = this.getMulti();
+    delete multi[key];
     this.setObject(MULTI_STORE_KEY, multi);
   }
 
